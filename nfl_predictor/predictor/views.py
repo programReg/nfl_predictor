a2c58273ab539d0ada821797805990ca68d812c2
programReg <1reggiejoseph@gmail.com>
2024-09-20 04:33:37 -0400

    Updated path for CSV file loading

--- nfl_predictor/predictor/views.py
+++ nfl_predictor/predictor/views.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+from django.shortcuts import render
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
