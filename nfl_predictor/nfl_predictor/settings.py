4ddb4b14004143806f41850e2cb8ff6d19bc44dc
programReg <1reggiejoseph@gmail.com>
2024-09-22 02:34:26 -0400

    final

--- nfl_predictor/nfl_predictor/settings.py
+++ nfl_predictor/nfl_predictor/settings.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import os
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -17,6 +16,7 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['nfl-predictor.onrender.com', '.onrender.com']
 
+DATABASES = {}
 
 
 # Application definition
