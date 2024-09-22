198b609ca0d219ce3b3bd5d2aa5d762ad1df58b7
programReg <1reggiejoseph@gmail.com>
2024-09-21 21:52:59 -0400

    final

--- nfl_predictor/nfl_predictor/settings.py
+++ nfl_predictor/nfl_predictor/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "https://nfl-predictor-rho.vercel.app",
+]
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
